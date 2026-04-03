ea86cd6e25a5ecf94ef294cc29ebdbea3ea6cbc6
Juan Gutierrez <juan.gutierrez0157@gmail.com>
2026-04-02 21:40:26 -0400

    refactor(service-worker): for easier versioning

--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,42 @@
+const REPOSITORY = "exchange-calculator";
+const CACHE_VERSION = "v2"
+const URLS = [
+    "/",
+    "/favicon.ico",
+    "/index.html",
+    "/main.js",
+    "/style.css",
+].map((url) => `/${REPOSITORY}${url}`);
+
 self.addEventListener("install", (event) => {
     event.waitUntil(
-        caches.open("exchange-calculator_v1").then((cache) =>
-            cache.addAll([
-                "/exchange-calculator/",
-                "/exchange-calculator/favicon.ico",
-                "/exchange-calculator/index.html",
-                "/exchange-calculator/main.js",
-                "/exchange-calculator/style.css",
-            ])
+        caches.open(`${REPOSITORY}_${CACHE_VERSION}`).then((cache) =>
+            cache.addAll(URLS)
         )
     );
 });
 
-self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) =>
-      response ?? fetch(event.request)
+self.addEventListener("activate", (event) => {
+    event.waitUntil(
+        caches.keys().then((keys) => Promise.all(
+            keys.filter((key) =>
+                key.startsWith(REPOSITORY) && !key.endsWith(CACHE_VERSION)
+            ).map((key) =>
+                caches.delete(key)
+            )  
+        ))
     )
-  );
+});
+
+self.addEventListener("fetch", (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) =>
+            response ?? fetch(event.request).catch((error) => 
+                new Response(error, {
+                    status: 408,
+                    headers: {"Content-Type": "text/plain"}
+                })
+            )
+        )
+    );
 });
